a071bbf6243b273cec8e5df9f163006aeb6ae149
ElCapitan <dee.jay.bhx@gmail.com>
2024-03-12 12:28:23 +0000

    azure server init added

--- src/admin/DB.php
+++ src/admin/DB.php
@@ -1,12 +1,13 @@
 <?php
 
+$con = mysqli_init();
 $host = 'auctionhouse-db.mysql.database.azure.com';
 $port = 3306;
 $user = 'auctionhouse_user';
 $pass = 'auction12345';
 $dbname = 'auctionhouse_db';
 
-$conn = mysqli_connect($host,$user,$pass,$dbname);
+$conn = mysqli_real_connect($con,$host,$user,$pass,$dbname,$port);
 
 if(!$conn){
     die("Database Connection Failed " . mysqli_connect_error());
